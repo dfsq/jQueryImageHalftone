7c5d17c89162d88a46041fc5cad4c445c037d9c1
Aliaksandr Astashenkau <dfsq.dfsq@gmail.com>
2011-08-14 19:32:29 +0300

    AM-dithering removed. Plugin simplified and finished.

--- README.md
+++ README.md
@@ -1,3 +1,6 @@
 # jQuery imageHalftone plugin
 
 Floyd-Steinberg algorithm implementation for javascript.
+Here is the example of image conversion:
+
+![halftoned](http://dfsq.info/media/images/halftoned.png)
--- jquery.imagehalftone.js
+++ jquery.imagehalftone.js
@@ -1,17 +1,29 @@
 /**
- * Steps:
- * 1. Find images.
- * 2. Replace original image with new haltone version.
+ * Simple image halftone Floyd-Steinberg dithering algorithm implementation.
+ * Converts image into two color halftoned.
+ *
+ * @author Aliaksandr Astashenkau, dfsq.dfsq@gmail.com.
+ *
+ * Options:
+ * radius: [Integer:1] Number of pixels per dot.
+ * output: [String:'canvas'] Type of resulting image representation.
+ *     canvas - original image is replaced with canvas element.
+ *     image  - original image is replaced with a new image.
+ * target: [String:null] CSS selector of the container to append new converted
+ *     image (or canvas) to. Original image will not be replaced.
+ * grayscale: [Boolean:false] Just convert image to grayscale.
+ * addCSS: [Object:{}] CSS properties to be added to the image or canvas.
+ * addClass: [String:null] Class name to be added.
  */
 ;(function($) {
 	$.fn.imageHalftone = function(options) {
-		/**
-		 * Default haltone image options.
-		 * For radius 1 algorithm behave like Floyd-Steinberg dithering.
-		 * For radius > 1 AM dithering.
-		 */
-		var options = $.extend({
-			radius: 1
+		var settings = $.extend({
+			radius: 1,
+			output: 'canvas',
+			target: null,
+			grayscale: false,
+			addCSS: {},
+			addClass: null
 		}, options);
 
 		/**
@@ -39,70 +51,66 @@
 					// Array of image color pixels
 					this.raw = this.ctx.getImageData(0, 0, this.canvas.attr('width'), this.canvas.attr('height'));
 
+					console.time('process');
 					this.process();
+					console.timeEnd('process');
 
-					// TODO temp append canvas to document
-					this.canvas.appendTo('body');
-				},
+					// New element
+					var $el = settings.output == 'canvas'
+						? this.canvas
+						: (function(c) {
+							var i = new Image();
+							i.src = c.toDataURL("image/png");
+							return i;
+						})(this.canvas[0]);
 
-				drawCircle: function(i, j, gray) {
-					var saturation = this.saturation(gray);
-					var r = options.radius;
+					settings.addClass && $el.addClass(settings.addClass);
+					$el.css(settings.addCSS);
 
-					// Radius of the circle to be placed in the section
-					var R = Math.round(r * Math.sqrt(saturation / Math.PI));
-
-					// Draw square first
-					this.ctx.fillStyle = '#FFF';
-					this.ctx.fillRect(i, j, r, r);
-
-					// Draw circle
-					this.ctx.fillStyle = '#000';
-					this.ctx.beginPath();
-					this.ctx.arc(i + r/2, j + r/2, R, 0, 2 * Math.PI);
-					this.ctx.closePath();
-					this.ctx.fill();
+					settings.target ? $(settings.target).append($el) : $img.replaceWith($el);
 				},
 
 				/**
 				 * Getter/setter of the average color of the pixel/section.
 				 * Get the average color of the section refered by its vertical and
 				 * horizontal index in the array matrix. Section can be either 1x1 pixel
-				 * or squares like 16x16 pixels. Size of the section is defined by radius setting.
-				 * @param x int coordinates of the left upper corner pixel
-				 * @param y int coordinates of the left upper corner pixel
-				 * @param radius
-				 * @param value int a new color to assign to pixel (section); if defined method acts as setter.
+				 * or squares like 16x16 pixels. Size of the section is defined by r setting.
+				 * @param i int coordinates of the left upper corner pixel
+				 * @param j int coordinates of the left upper corner pixel
+				 * @param r size of the dot
+				 * @param val int a new color to assign to pixel (section); if defined method acts as setter.
 				 */
-				area: function(i, j, radius, value, gray) {
+				area: function(i, j, r, val) {
 
-					if (radius == 1) {
+					var d = this.raw.data;
+
+					if (r == 1) {
 						var index = i * 4 + j * 4 * this.raw.width;
-						if (typeof value == 'undefined') {
-							return (this.raw.data[index] + this.raw.data[index + 1] + this.raw.data[index + 2]) / 3;
+						if (typeof val == 'undefined') {
+							return (d[index] + d[index + 1] + d[index + 2]) / 3;
 						}
 						else {
-							value = Math.round(value);
-							this.raw.data[index] = value;
-							this.raw.data[index + 1] = value;
-							this.raw.data[index + 2] = value;
+							val = Math.round(val);
+							d[index] = val;
+							d[index + 1] = val;
+							d[index + 2] = val;
 						}
 					}
 					else {
-						if (typeof value == 'undefined') {
+						var ri, rj;
+
+						if (typeof val == 'undefined') {
 							var average = 0;
-							for (var ri = 0; ri < radius; ri++)
-								for (var rj = 0; rj < radius; rj++)
+							for (ri = 0; ri < r; ri++)
+								for (rj = 0; rj < r; rj++)
 									average += this.area(i + ri, j + rj, 1);
 
-							return average/(radius*radius);
+							return average/(r*r);
 						}
 						else {
-//							for (var ri = 0; ri < radius; ri++)
-//								for (var rj = 0; rj < radius; rj++)
-//									this.area(i + ri, j + rj, 1, value);
-
-							this.drawCircle(i, j, gray);
+							for (ri = 0; ri < r; ri++)
+								for (rj = 0; rj < r; rj++)
+									this.area(i + ri, j + rj, 1, val);
 						}
 					}
 				},
@@ -115,39 +123,35 @@
 					return color < 128 ? 0 : 255;
 				},
 
-				/**
-				 * Define how dark the gray color is. 0 - means 100% black.
-				 * @param color
-				 */
-				saturation: function(color) {
-					return (255 - color) / 255;
-				},
-
 				process: function() {
 					// Iteration delta
-					var d = options.radius;
+					var r = settings.radius,
+						w = this.raw.width,
+						h = this.raw.height;
 
 					// Iterate over each pixel in array
-					for (var i = 0; i < this.raw.height; i = i + d) {
-						for (var j = 0; j < this.raw.width; j = j + d) {
+					for (var i = 0; i < h; i = i + r) {
+						for (var j = 0; j < w; j = j + r) {
 							// Old pixel converted to grayscale
-							var oldPixel = this.area(i, j, d);
+							var oldPixel = newPixel = this.area(i, j, r);
 
-							// Change color accordeing to how dark gray is
-							var newPixel = this.closestColor(oldPixel);
+							if (!settings.grayscale) {
+								// Change color accordeing to how dark gray is
+								var newPixel = this.closestColor(oldPixel);
+
+								var quantError = oldPixel - newPixel;
+								j+r < w && this.area(i, j+r, r, this.area(i, j+r, r) + (7/16)*quantError);
+								i+r < h && j > r && this.area(i+r, j-r, r, this.area(i+r, j-r, r) + (3/16)*quantError);
+								i+r < h && this.area(i+r, j, r, this.area(i+r, j, r) + (5/16)*quantError);
+								i+r < h && j+r < w && this.area(i+r, j+r, r, this.area(i+r, j+r, r) + (1/16)*quantError);
+							}
 
 							// Set new pixel
-							this.area(i, j, d, newPixel, oldPixel);
-
-							var quantError = oldPixel - newPixel;
-							j+d < this.raw.width  && this.area(i, j+d, d, this.area(i, j+d, d) + (7/16)*quantError);
-							i+d < this.raw.height && j > d && this.area(i+d, j-d, d, this.area(i+d, j-d, d) + (3/16)*quantError);
-							i+d < this.raw.height && this.area(i+d, j, d, this.area(i+d, j, d) + (5/16)*quantError);
-							i+d < this.raw.height && j+d < this.raw.width && this.area(i+d, j+d, d, this.area(i+d, j+d, d) + (1/16)*quantError);
+							this.area(i, j, r, newPixel);
 						}
 					}
 
-					//this.ctx.putImageData(this.raw, 0, 0, 0, 0, this.raw.width, this.raw.height);
+					this.ctx.putImageData(this.raw, 0, 0, 0, 0, w, h);
 				}
 			};
 		};
